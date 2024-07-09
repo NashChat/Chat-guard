4735266206f3ad866aee96938849083ab1ed861f
Orya Mert Kansız <164668378+NashChat@users.noreply.github.com>
2024-07-09 14:24:56 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = { 
+//------------Connections-----------//
+    Client_Token: '',
+    MongoDB_ConnectURL: '',
+    //------------BotSettings-----------//
+    Prefix: '',
+    BotOwners: [""],
+    Custom_Status_Text: 'hey',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'dnd', // => dnd / idle / online / invisible
+    VoiceChannelID: ''
+};
+  
+module.exports = config;
